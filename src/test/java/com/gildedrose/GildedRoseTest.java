2fafd6051069be7a59d21cea2c31e9b5a62ea364
Nicolas Pascal <nicolas.pascal@online-pajak.com>
2020-09-18 10:19:54 +1000

    added first test

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,5 +1,6 @@
 package com.gildedrose;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -7,11 +8,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    @DisplayName("regular Items should decrease by 1 in quality and sellin if not expired")
+    void regularItemNotExpired() {
+        // GIVEN
+        Item[] items = new Item[] { new Item("Regular Item", 10, 5) };
+
         GildedRose app = new GildedRose(items);
+
+        // WHEN
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+
+        // THEN
+        assertEquals("Regular Item", app.items[0].name);
+        assertEquals(9, app.items[0].sellIn);
+        assertEquals(4, app.items[0].quality);
     }
 
 }
